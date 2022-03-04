fbc5ac0d7ad79156c1d577e1fc742bc78b8979e3
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2022-03-04 21:57:18 +0530

    First initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import GlobalStyle from "./components/GlobalStyle";
+import { CountriesProvider } from "./context/CountriesContext";
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import Navbar from "./components/navbar/Navbar";
+import { ThemeProvider } from "styled-components";
+
+const LightTheme = {
+  pageBackground: "hsl(0, 0%, 98%)",
+  titleColor: "hsl(200, 15%, 8%)",
+  navBackground: "hsl(0, 0%, 100%)",
+  searchBackground: "hsl(0, 0%, 100%)",
+  searchInput: "hsl(0, 0%, 52%)",
+  searchColor: "hsl(0, 0%, 52%)",
+};
+
+const DarkTheme = {
+  pageBackground: "hsl(207, 26%, 17%)",
+  titleColor: "hsl(0, 0%, 100%)",
+  navBackground: "hsl(209, 23%, 22%)",
+  searchBackground: "hsl(209, 23%, 22%)",
+  searchInput: "hsl(0, 0%, 100%)",
+  searchColor: "hsl(0, 0%, 100%)",
+};
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+};
 
 function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <GlobalStyle />
+      <CountriesProvider>
+        <Router>
+          <Navbar theme={theme} setTheme={setTheme} />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/details/:name" element={<DetailsPage />} />
+          </Routes>
+        </Router>
+      </CountriesProvider>
+    </ThemeProvider>
   );
 }
 
